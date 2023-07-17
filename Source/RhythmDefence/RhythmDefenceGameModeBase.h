3a2fe05309199e3d9ddfaede855397b15138e0c4
LAPTOP-AR02UUCD\tigloos <wayding20012012@hotmail.com>
2023-07-17 11:25:27 +0300

    Initial Commit

---
+++ Source/RhythmDefence/RhythmDefenceGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RhythmDefenceGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RHYTHMDEFENCE_API ARhythmDefenceGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
